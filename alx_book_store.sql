634b087e8250988d286ed9b06fd3b38c6932af75
Lozwana <mabasohappyness@gmail.com>
2025-07-23 01:13:49 +0200

    corrected data type

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_details INT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
