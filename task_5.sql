962d397402684493d7aa5cdc84924fac99909caf
Lozwana <mabasohappyness@gmail.com>
2025-07-25 04:30:24 +0200

    create and pupulate table

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,16 @@
 -- task_5.sql
--- This SQL script is designed to insert a new row into the 'Customers' table in the database.
+-- This SQL script is designed to create a 'customer' table and
+-- insert a new customer into the table.
+-- The table will have fields for customer_id, customer_name, email, and address.
+
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100),
+    email VARCHAR(100),
+    address VARCHAR(255)
+);
+-- Inserts a new customer into the 'customer' table
+-- The customer_id is set to 1, and the other fields are filled with sample data
 INSERT INTO customer (
     customer_id,
     customer_name,
@@ -13,8 +24,4 @@ VALUES (
     'cbaidoo@sandtech.com',
     '123 Happiness Ave.'
 )
--- Uses ON DUPLICATE KEY UPDATE clause to handle duplicate primary keys
-/* ON DUPLICATE KEY UPDATE
-    customer_name = VALUES(customer_name),
-    email = VALUES(email),
-    address = VALUES(address);
+ON CONFLICT (customer_id) DO NOTHING;
